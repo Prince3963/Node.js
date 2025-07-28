14d91ce2aecfac8e8b1cac33caad0b9ea9966fe2
adirpathak7 <adityarpathak7@gmail.com>
2025-07-29 01:22:44 +0530

    Problem 11 completed

---
+++ Problem11/Test.js
@@ -0,0 +1,6 @@
+const utils = require('./app');
+
+console.log(utils.capitalize('hello'));
+console.log(utils.reverse('hello'));
+console.log(utils.isPalindrome('madam'));
+console.log(utils.isPalindrome('hello'));
